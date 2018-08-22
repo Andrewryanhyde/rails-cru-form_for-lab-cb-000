2f4ed1da71a9ad9600200dad0bd2fcbd8c47dc02
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-22 11:25:31 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,25 +1,9 @@
 class GenresController < ApplicationController
-  def create
-    @genre = Genre.new(genre_params(:name, :bio))
-    @genre.save
-    redirect_to genre_path(@genre)
-  end
-
-  def show
-    @genre = Genre.find(params[:id])
-  end
-
-  def update
-    @genre = Genre.find(params[:id])
-    @genre.update(genre_params(:name, :bio))
-    redirect_to genre_path(@genre)
-  end
-
   def index
     @genres = Genre.all
   end
 
-  def edit
+  def show
     @genre = Genre.find_by_id(params[:id])
   end
 
@@ -27,9 +11,19 @@ class GenresController < ApplicationController
     @genre = Genre.new
   end
 
-  private
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
 
-  def genre_params(*args)
-    params.require(:genre).permit(*args)
+  def edit
+    @genre = Genre.find_by_id(params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by_id(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 end
