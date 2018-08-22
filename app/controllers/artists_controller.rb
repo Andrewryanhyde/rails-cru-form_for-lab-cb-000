62295f4b9f50da0fa46146a303041a2f4a0f4a3c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-22 10:42:10 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,12 @@
 class ArtistsController < ApplicationController
 
   def create
+    @artist = Artist.create(name: params[:name], bio: params[:bio])
+    redirect_to artist_path(@artist)
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def update
