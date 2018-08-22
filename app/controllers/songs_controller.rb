7607e6f79c19f65c7403a92fee0c791cebdcbb7e
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-22 10:49:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,24 @@
-class SongsController < ApplicationController
-  def create
+class SongsController < ApplicationController  def create
+    @song = Song.new(song_params(:name, :bio))
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :bio))
+    redirect_to song_path(@song)
+  end
+
+
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 end
