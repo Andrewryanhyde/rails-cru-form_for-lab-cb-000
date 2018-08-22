15204ff52e03d6c142e3e689b9582846e9fd0b1b
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-22 10:48:51 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,10 +1,25 @@
 class GenresController < ApplicationController
   def create
+    @genre = Genre.new(genre_params(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def show
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params(:name, :bio))
+    redirect_to genre_path(@genre)
+  end
+
+
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
   end
 end
